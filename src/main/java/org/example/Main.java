b7e754943e7fb4c67aacb711f405c56bab3df019
Alfani_Alif <shofwanaliffigma18@gmail.com>
2024-02-20 19:15:47 +0700

    nambahin loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("hello world");
+        System.out.printf("hello world\n");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("world hello\n");
+        }
     }
 }
