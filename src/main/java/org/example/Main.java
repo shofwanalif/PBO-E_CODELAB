97506f734790e39a512c9114fc7ac692bfd5e64d
Alfani_Alif <shofwanaliffigma18@gmail.com>
2024-02-20 21:47:34 +0700

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,26 @@
 package org.example;
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("hello world\n");
+        String nama;
+        String tgllahir;
+        String jeniskelamin;
+        int umur;
+        Scanner objInput = new Scanner(System.in);
 
-        for (int i = 0; i < 5; i++) {
-            System.out.printf("world hello\n");
-        }
+        System.out.print("Masukkan nama: ");
+        nama = objInput.nextLine();
+
+        System.out.print("Masukkan jenis kelamin: ");
+        jeniskelamin = objInput.nextLine();
+
+        System.out.print("Masukkan tanggal lahir (YYY-MM-DD): ");
+        tgllahir = objInput.nextLine();
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Jenis Kelamin: " + jeniskelamin);
     }
 }
